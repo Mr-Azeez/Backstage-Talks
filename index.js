8849b839faae9704d31051b8945570a6708f8754
Abdulazeez <abolurinazeez2005@gmail.com>
2023-12-21 20:46:23 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+function issue6(z) {
+  var a = getComputedStyle(z);
+  var b = a.backgroundColor;
+  document.getElementsByTagName("BODY")[0].style.backgroundColor = b;
+}
+function issue5(z) {
+  var a = getComputedStyle(z);
+  var b = a.backgroundColor;
+  document.getElementsByTagName("BODY")[0].style.backgroundColor = b;
+}
+function issue4(z) {
+  var a = getComputedStyle(z);
+  var b = a.backgroundColor;
+  document.getElementsByTagName("BODY")[0].style.backgroundColor = b;
+}
+function issue3(z) {
+  var a = getComputedStyle(z);
+  var b = a.backgroundColor;
+  document.getElementsByTagName("BODY")[0].style.backgroundColor = b;
+}
+function issue2(z) {
+  var a = getComputedStyle(z);
+  var b = a.backgroundColor;
+  document.getElementsByTagName("BODY")[0].style.backgroundColor = b;
+}
+function issue1(z) {
+  var a = getComputedStyle(z);
+  var b = a.backgroundColor;
+  document.getElementsByTagName("BODY")[0].style.backgroundColor = b;
+}
+
+function scroll6(z) {
+  var a = getComputedStyle(z);
+  var b = a.backgroundColor;
+  document.getElementsByTagName("BODY")[0].style.backgroundColor = b;
+}
+function scroll5(z) {
+  var a = getComputedStyle(z);
+  var b = a.backgroundColor;
+  document.getElementsByTagName("BODY")[0].style.backgroundColor = b;
+}
+function scroll4(z) {
+  var a = getComputedStyle(z);
+  var b = a.backgroundColor;
+  document.getElementsByTagName("BODY")[0].style.backgroundColor = b;
+}
+function scroll3(z) {
+  var a = getComputedStyle(z);
+  var b = a.backgroundColor;
+  document.getElementsByTagName("BODY")[0].style.backgroundColor = b;
+}
+function scroll2(z) {
+  var a = getComputedStyle(z);
+  var b = a.backgroundColor;
+  document.getElementsByTagName("BODY")[0].style.backgroundColor = b;
+}
+function scroll1(z) {
+  var a = getComputedStyle(z);
+  var b = a.backgroundColor;
+  document.getElementsByTagName("BODY")[0].style.backgroundColor = b;
+}
